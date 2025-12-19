3cc1b47c67870e105515fa1d2abe854c531d2809
Kojrey <kojrey@kojreycodes.com>
2025-12-20 02:29:42 +1100

    MVP URL Routing and View/Template creation

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    """Home page displaying charts and odds tables."""
+    return render(request, "polls/home.html")
+
+
+def about(request):
+    """About page with information about BetPoll."""
+    return render(request, "polls/about.html")
